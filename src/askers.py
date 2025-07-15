dda31c9427264f7d76ee254cfdfe15d28e0f00b7
Mr Chamster <s.duda03@gmail.com>
2025-07-16 00:07:13 +0200

    ask_initial def

--- src/askers.py
+++ src/askers.py
@@ -3,15 +3,38 @@ import os
 
 
 
+def ask_initial():
+    returns_dict = {"ftf": "file_to_file",
+                    "ftd": "file_to_dir",
+                    "dtf": "dir_to_file",
+                    "dtd": "dir_to_dir"}
+
+    while True:
+        print("Choose action:\n" \
+            "ftf  - Image file to audio file\n" \
+            "ftd  - Image file to audio directory\n" \
+            "dtf  - Image directory to audio file\n" \
+            "dtd  - Image directory to audio directory\n" \
+            "exit - Exit program\n>> ", end="")
+        asker = input()
+
+        if asker == "exit":
+            return None
+        elif asker not in ["ftf", "ftd", "dtf", "dtd"]:
+            print("Incorrect input.\n")
+        else:
+            return returns_dict[asker]
+
+
 def ask_embed_or_remove():
     returns_dict = {"e": "embed",
                     "r": "remove"}
 
     while True:
         print("Choose images embedding or removal:\n" \
-            "(input 'exit' to exit)\n" \
-            "e - embedding\n" \
-            "r - removal\n>> ", end="")
+              "(input 'exit' to exit)\n" \
+              "e - embedding\n" \
+              "r - removal\n>> ", end="")
         asker = input()
 
         if asker == "exit":
