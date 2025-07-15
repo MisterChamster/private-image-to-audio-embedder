6aa595f2f102737aacbbd125fe1d710ceffa5a55
Mr Chamster <s.duda03@gmail.com>
2025-07-16 01:19:17 +0200

    Code simplification

--- src/file_operations/general.py
+++ src/file_operations/general.py
@@ -3,28 +3,14 @@ from os import path, listdir
 
 
 def has_img_extension(filename):
-    """
-    Checks if file has jpg, png or jpeg extension.
-
-    Args:
-        filename (str): Name of a file.
-    Returns:
-        (boolean): If file is an image or not.
-    """
-    if filename[-3:] in ["png", "jpg"] or filename.endswith("jpeg"):
+    if (filename.endswith("png") or
+        filename.endswith("jpg") or
+        filename.endswith("jpeg")):
         return True
     return False
 
 
 def get_extension(filename):
-    """
-    Returns extension of a file.
-    
-    Args:
-        filename (str): Name of a file from which extension will be got.
-    Returns:
-        ext (str): File extension.
-    """
     return filename.split(".")[-1]
 
 
