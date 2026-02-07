af9e809e608b74ac9acd1b94c0e7e2d1a166800d
Szymon Duda <91761037+MisterChamster@users.noreply.github.com>
2026-02-07 16:41:05 +0100

    Removal routes module

--- src/main_loop.py
+++ src/main_loop.py
@@ -4,9 +4,7 @@ from src.routes.img_file_to_audio_dir import img_file_to_audio_dir
 from src.routes.img_dir_to_audio_file import img_dir_to_audio_file
 from src.routes.img_dir_to_audio_dir import img_dir_to_audio_dir
 from src.routes.img_dir_to_audio_dir_recur import img_dir_to_audio_dir_recur
-from src.routes.remove_from_audio_file import remove_from_audio_file
-from src.routes.remove_from_audio_dir import remove_from_audio_dir
-from src.routes.remove_from_audio_dir_recur import remove_from_audio_dir_recur
+import src.routes.removal_routes as rmroutes
 
 
 
@@ -38,10 +36,10 @@ def main_loop() -> None:
             img_dir_to_audio_dir_recur()
 
         elif action == "remove_from_audio_file":
-            remove_from_audio_file()
+            rmroutes.remove_from_audio_file()
 
         elif action == "remove_from_audio_dir":
-            remove_from_audio_dir()
+            rmroutes.remove_from_audio_dir()
 
         elif action == "remove_from_audio_dir_recur":
-            remove_from_audio_dir_recur()
+            rmroutes.remove_from_audio_dir_recur()
---
+++ src/routes/removal_routes.py
@@ -0,0 +1,31 @@
+from src.img_to_audio.audio_dir_tools import AudioDirTools
+from src.img_to_audio.audio_file_tools import AudioFileTools
+from src.askers import Askers
+
+
+
+def remove_from_audio_file() -> None:
+    print("Choose audio file")
+    input_audio_file_path = Askers.ask_path_filedialog("f", "Audio file path")
+    print(f"Path chosen: {input_audio_file_path}\n")
+
+    AudioFileTools.remove_image(input_audio_file_path)
+    print("Image succesfully removed\n\n")
+
+
+def remove_from_audio_dir() -> None:
+    print("Choose audio directory")
+    input_audio_dir_path = Askers.ask_path_filedialog("d", "Audio directory path")
+    print(f"Path chosen: {input_audio_dir_path}\n")
+
+    AudioDirTools.remove_images_dir(input_audio_dir_path)
+    print("Images succesfully removed\n\n")
+
+
+def remove_from_audio_dir_recur() -> None:
+    print("Choose audio directory")
+    input_audio_dir_path = Askers.ask_path_filedialog("d", "Audio directory path")
+    print(f"Path chosen: {input_audio_dir_path}\n")
+
+    AudioDirTools.remove_images_recursion(input_audio_dir_path)
+    print("Images succesfully removed\n\n")
