808c81c1754a6d1b9d584c993862d6b8f6b1d49d
Szymon Duda <91761037+MisterChamster@users.noreply.github.com>
2026-03-20 18:17:01 +0100

    Bug fixing after testing

--- src/routes/removal_routes.py
+++ src/routes/removal_routes.py
@@ -6,7 +6,7 @@ from src.img_to_audio.audio_file_tools import AudioFileTools
 
 def remove_from_audio_file() -> None:
     print("Choose audio file")
-    audio_file_path = Askers.ask_path_filedialog("f", "Audio file path")
+    audio_file_path = Askers.ask_path_filedialog("file", "audio")
     if not audio_file_path:
         print("\n")
         return
@@ -19,7 +19,7 @@ def remove_from_audio_file() -> None:
 
 def remove_from_audio_dir() -> None:
     print("Choose audio directory")
-    audio_dir_path = Askers.ask_path_filedialog("d", "Audio directory path")
+    audio_dir_path = Askers.ask_path_filedialog("dir", "audio")
     if not audio_dir_path:
         print("\n")
         return
@@ -32,7 +32,7 @@ def remove_from_audio_dir() -> None:
 
 def remove_from_audio_dir_recur() -> None:
     print("Choose audio directory")
-    audio_dir_path = Askers.ask_path_filedialog("d", "Audio directory path")
+    audio_dir_path = Askers.ask_path_filedialog("dir", "audio")
     if not audio_dir_path:
         print("\n")
         return
